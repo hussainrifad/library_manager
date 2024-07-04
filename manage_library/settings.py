d152e98ed6ba965be109b6babee65d02cfde17a1
Muhammad Abu Hussain Rifad <hussain.rifad89@gmail.com>
2024-07-04 12:34:47 +0300

    added host

--- manage_library/settings.py
+++ manage_library/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5=m1urq95yx^5axqhp6@2m&_b*s63p@1kucmo=^*x@$c@7(vhl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
