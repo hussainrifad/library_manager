8b22a5d455308e92e8f8e23b19cd57ad6cfe97bc
Muhammad Abu Hussain Rifad <hussain.rifad89@gmail.com>
2024-07-08 10:02:04 +0300

    migrate done

--- manage_library/settings.py
+++ manage_library/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,11 +83,28 @@ WSGI_APPLICATION = 'manage_library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'bear_library',
+#        'USER': 'postgres',
+#        'PASSWORD': 'wtpmjgda',
+#        'HOST': 'localhost',
+#        'PORT': '8000',
+#    }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://bearllibrary_user:n4DcYMJMVc1vGGIKng1aE4VmTeDxAofG@dpg-cq5oiq88fa8c738abs50-a.oregon-postgres.render.com/bearllibrary',
+    )
 }
 
 
